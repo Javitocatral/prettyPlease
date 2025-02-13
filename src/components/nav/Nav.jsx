6c45f0f0c0e870d2a070fd051cfa81788cdc4b9b
Javier Gascon <javitocatral@gmail.com>
2025-02-13 08:52:25 +0100

    menu para mobil

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,49 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import './nav.css'
+import './Nav.css'
 
 function Nav() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <nav>
-      <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
-        Home
-      </NavLink>
-      <NavLink
-        to="/step"
-        className={({ isActive }) => (isActive ? 'active' : '')}
-      >
-        Steps
-      </NavLink>
-      <NavLink
-        to="/choreos"
-        className={({ isActive }) => (isActive ? 'active' : '')}
-      >
-        Choreos
-      </NavLink>
-      <NavLink
-        to="/shop"
-        className={({ isActive }) => (isActive ? 'active' : '')}
-      >
-        Shop
-      </NavLink>
-      <NavLink
-        to="/events"
-        className={({ isActive }) => (isActive ? 'active' : '')}
-      >
-        Events
-      </NavLink>
-      <NavLink
-        to="/maintenance"
-        className={({ isActive }) => (isActive ? 'active' : '')}
-      >
-        Maintenance
-      </NavLink>
-      <NavLink
-        to="/about"
-        className={({ isActive }) => (isActive ? 'active' : '')}
-      >
-        About
-      </NavLink>
+    <nav className="navbar">
+      <div className="hamburger" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+      <div className={`nav-links ${isOpen ? 'open' : ''}`}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? 'active' : '')}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/step"
+          className={({ isActive }) => (isActive ? 'active' : '')}
+        >
+          Steps
+        </NavLink>
+        <NavLink
+          to="/choreos"
+          className={({ isActive }) => (isActive ? 'active' : '')}
+        >
+          Choreos
+        </NavLink>
+        <NavLink
+          to="/shop"
+          className={({ isActive }) => (isActive ? 'active' : '')}
+        >
+          Shop
+        </NavLink>
+        <NavLink
+          to="/events"
+          className={({ isActive }) => (isActive ? 'active' : '')}
+        >
+          Events
+        </NavLink>
+        <NavLink
+          to="/maintenance"
+          className={({ isActive }) => (isActive ? 'active' : '')}
+        >
+          Maintenance
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? 'active' : '')}
+        >
+          About
+        </NavLink>
+      </div>
     </nav>
   )
 }
