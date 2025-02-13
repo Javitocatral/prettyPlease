a37ebb06cf0dbccd521a3ef824e9a133fb6a7f3d
Javier Gascon <javitocatral@gmail.com>
2025-02-13 13:35:48 +0100

    .env

--- firebase.js
+++ firebase.js
@@ -1,21 +1,22 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
+import { getAnalytics } from 'firebase/analytics'
 
 // Tu configuración de Firebase
 const firebaseConfig = {
-  apiKey: 'AIzaSyCNsX5IsEjQuDqN9hx7Bbdi7Yf22InxXxg',
-  authDomain: 'prettyplease-61d1d.firebaseapp.com',
-  projectId: 'prettyplease-61d1d',
-  storageBucket: 'prettyplease-61d1d.firebasestorage.app',
-  messagingSenderId: '950907527554',
-  appId: '1:950907527554:web:e4f0c124eb56e56d1c6f9b',
-  measurementId: 'G-7FF11BQ68M',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 }
-
 // Inicializa Firebase
 const app = initializeApp(firebaseConfig)
 
 // Exporta las instancias de Firestore y Auth
 export const db = getFirestore(app)
 export const auth = getAuth(app)
+const analytics = getAnalytics(app)
