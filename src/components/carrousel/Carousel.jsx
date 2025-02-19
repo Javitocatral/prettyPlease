a2250865882763a31b102178c2c3aa2f00c5220e
Javier Gascon <javitocatral@gmail.com>
2025-02-19 16:42:23 +0100

    links carousel

--- src/components/carrousel/Carousel.jsx
+++ src/components/carrousel/Carousel.jsx
@@ -4,6 +4,7 @@ import roller1 from '../../assets/roller1.jpg'
 import roller2 from '../../assets/roller2.jpg'
 import roller3 from '../../assets/roller3.jpg'
 import roller4 from '../../assets/roller4.png'
+import { Link } from 'react-router-dom'
 
 const text1Options = [
   'Tips, maintenance and much more',
@@ -17,6 +18,7 @@ const text2Options = ['Maintenance', 'Choreos', 'Steps', 'Events']
 const colorOptions = ['#27feef', '#ff46a4', '#fcf92c', '#bb64c6']
 
 const imageOptions = [roller1, roller2, roller3, roller4]
+const links = ['/maintenance', '/choreos', '/steps', '/events']
 
 const Carousel = () => {
   const [index, setIndex] = useState(0)
@@ -51,40 +53,42 @@ const Carousel = () => {
     }, 650)
   }
   return (
-    <div id="carousel-wrapper" className={animation}>
-      <div id="menu" style={{ background: colorOptions[index] }}>
-        <div id="current-option">
-          <div
-            id="current-option-text1"
-            data-next-text={text1Options[(index + 1) % text1Options.length]}
-            data-previous-text={
-              text1Options[
-                (index - 1 + text1Options.length) % text1Options.length
-              ]
-            }
-          >
-            {text1Options[index]}
+    <Link to={links[index]}>
+      <div id="carousel-wrapper" className={animation}>
+        <div id="menu" style={{ background: colorOptions[index] }}>
+          <div id="current-option">
+            <div
+              id="current-option-text1"
+              data-next-text={text1Options[(index + 1) % text1Options.length]}
+              data-previous-text={
+                text1Options[
+                  (index - 1 + text1Options.length) % text1Options.length
+                ]
+              }
+            >
+              {text1Options[index]}
+            </div>
+            <div
+              id="current-option-text2"
+              data-next-text={text2Options[(index + 1) % text2Options.length]}
+              data-previous-text={
+                text2Options[
+                  (index - 1 + text2Options.length) % text2Options.length
+                ]
+              }
+            >
+              {text2Options[index]}
+            </div>
           </div>
-          <div
-            id="current-option-text2"
-            data-next-text={text2Options[(index + 1) % text2Options.length]}
-            data-previous-text={
-              text2Options[
-                (index - 1 + text2Options.length) % text2Options.length
-              ]
-            }
-          >
-            {text2Options[index]}
-          </div>
+          <button id="previous-option" onClick={handlePrevious}></button>
+          <button id="next-option" onClick={handleNext}></button>
         </div>
-        <button id="previous-option" onClick={handlePrevious}></button>
-        <button id="next-option" onClick={handleNext}></button>
+        <div
+          id="image"
+          style={{ backgroundImage: `url(${imageOptions[index]})` }}
+        ></div>
       </div>
-      <div
-        id="image"
-        style={{ backgroundImage: `url(${imageOptions[index]})` }}
-      ></div>
-    </div>
+    </Link>
   )
 }
 
